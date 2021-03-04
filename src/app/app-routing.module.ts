a9ecb2fe99ca88e65133579353194fe3ba500475
Gregor Biswanger <gregor.biswanger@web-enliven.de>
2021-03-04 09:22:03 +0100

    Angular 11.2.4

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -36,7 +36,7 @@ const routes: Routes = [
 @NgModule({
   declarations: [],
   imports: [
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })
   ],
   exports: [RouterModule]
 })
